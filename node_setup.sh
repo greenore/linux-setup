567cdbf0869331aaed3126ea81a33fc61009bde6
Tim Hagmann <greenore@users.noreply.github.com>
2015-07-13 10:59:21 +0200

    Update node_setup.sh

--- node_setup.sh
+++ node_setup.sh
@@ -1,1 +1,58 @@
+#!/bin/bash
+
+## Add SSH 
+# ssh-add /location_to_the_key_file/xxxxxx.pem
+
+# System 
+#-------
+# Add apt-fast
+sudo add-apt-repository ppa:saiarcot895/myppa
+sudo apt-get update
+sudo apt-get install apt-fast
+
+# Upgrade system
+sudo apt-fast update
+sudo apt-fast upgrade
+sudo apt-fast dist-upgrade
+
+## Virtualbox Guest Additions (for optimal interaction)
+sudo apt-fast install virtualbox-guest-utils
+usermod -a -G vboxsf tim
+usermod -a -G vboxsf rstudio-server
+mkdir /home/tim/R
+ln -s /media/sf_R /home/tim
+# unlink /home/tim/sf_R
+
+## Configure Guests
+sudo nano /etc/network/interfaces
+
+## Add
+# The host-only network interface
+auto eth1
+iface eth1 inet static
+address 192.168.56.101
+netmask 255.255.255.0
+network 192.168.56.0
+broadcast 192.168.56.255
+
+# Set in virtualbox in the network settings the port forwarding
+# Host IP (leave emtpy):
+# Host Port: 8787
+# Guest IP: 192.168.56.101
+# Guest Port: 8787
+
+# Install ssh-server
+sudo apt-get install openssh-server
+
+# Installs Microsoft Core Fonts (Arial,Times New Roman and  many more)
+sudo apt-fast install ttf-mscorefonts-installer
+
+## Node.js
 sudo ln -s /usr/bin/nodejs /usr/local/bin/node
+
+
+# Cleanup
+#--------
+sudo apt-fast autoremove
+sudo apt-fast autoclean
+sudo apt-fast clean
