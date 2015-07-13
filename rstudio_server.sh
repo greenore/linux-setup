669a3365322fe51d6fec344daa8b820fc95d2716
Tim Hagmann <greenore@users.noreply.github.com>
2015-07-13 13:43:11 +0200

    Create rstudio_server.sh

---
+++ rstudio_server.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# System 
+#-------
+# Add apt-fast
+sudo add-apt-repository ppa:saiarcot895/myppa
+sudo apt-get update
+sudo apt-get install apt-fast
+
+# Upgrade system
+sudo apt-fast update
+sudo apt-fast upgrade
+sudo apt-fast dist-upgrade
+
+## Virtualbox Guest Additions (for optimal interaction)
+sudo apt-fast install virtualbox-guest-utils
+usermod -a -G vboxsf tim
+usermod -a -G vboxsf rstudio-server
+mkdir /home/tim/R
+ln -s /media/sf_R /home/tim
+# unlink /home/tim/sf_R
+
+## Configure Guests
+
+# Permamently
+sudo nano /etc/network/interfaces
+
+## Add
+# The host-only network interface
+auto eth1
+iface eth1 inet static
+address 192.168.56.101
+netmask 255.255.255.0
+network 192.168.56.0
+broadcast 192.168.56.255
+
+# Set in virtualbox in the network settings the port forwarding
+# Host IP (leave emtpy):
+# Host Port: 8787
+# Guest IP: 192.168.56.101
+# Guest Port: 8787
+
+# Install ssh-server
+sudo apt-get install openssh-server
+
+# Installs Microsoft Core Fonts (Arial,Times New Roman and  many more)
+sudo apt-fast install ttf-mscorefonts-installer
+
+## Data Science
+#---------------
+# Curl Dependencies
+sudo apt-fast install libcurl4-openssl-dev 
+
+# GEOS Dependencies
+sudo apt-fast install libgeos-dev
+
+# RODBC Dependencies
+sudo apt-fast install libiodbc2 libiodbc2-dev
+
+# R Dependencies
+sudo apt-fast install libxft-dev
+
+# Install R-Base
+sudo add-apt-repository ppa:marutter/rrutter
+sudo apt-fast update
+sudo apt-fast install r-base
+
+# RStudio Server
+sudo nano /etc/rstudio/rserver.conf
+rsession-which-r=/usr/lib64/RRO-3.2.0/R-3.2.0/bin/R
+
+# Install latex
+sudo apt-fast install texlive texlive-latex-extra
+
+# Install JAVA
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-fast update
+sudo apt-fast install oracle-java9-installer
+
+# Cleanup
+#--------
+sudo apt-fast autoremove
+sudo apt-fast autoclean
+sudo apt-fast clean
